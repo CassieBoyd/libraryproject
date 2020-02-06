16ad5a07009040c9c19043976b0d7b7b1036c4e0
Cassie Boyd <cassandra.bacon@gmail.com>
2020-02-06 16:03:34 -0600

    Created superuser

--- libraryapp/models/book.py
+++ libraryapp/models/book.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from .librarian import Librarian
 
 
 class Book (models.Model):
@@ -19,3 +19,5 @@ class Book (models.Model):
 
     def get_absolute_url(self):
         return reverse("book_detail", kwargs={"pk": self.pk})
+
+    librarian = models.ForeignKey(Librarian, on_delete=models.CASCADE)
