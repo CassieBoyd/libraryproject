fc30946f45300c711137c0e6d280beb9abd5e3a2
Cassie Boyd <cassandra.bacon@gmail.com>
2020-02-08 17:23:10 -0600

    Did Advanced portion of chapter 3 so the book form would work. Also changed staticfiles to just static in templates/books/form.html

--- libraryapp/views/books/list.py
+++ libraryapp/views/books/list.py
@@ -3,13 +3,16 @@ from django.shortcuts import render
 from libraryapp.models import Book
 from ..connection import Connection
 from django.contrib.auth.decorators import login_required
+from libraryapp.models import model_factory
+
 
 # Note that the row factory being used for this connection to the database uses the built-in sqlite3.Row method. This allows developers to access columns in each row in the dataset by the column name instead of by index in the tuple.
 @login_required
 def book_list(request):
     if request.method == 'GET':
         with sqlite3.connect(Connection.db_path) as conn:
-            conn.row_factory = sqlite3.Row
+            conn.row_factory = model_factory(Book)
+            # conn.row_factory = sqlite3.Row <-----Previous code
             db_cursor = conn.cursor()
 
             db_cursor.execute("""
@@ -24,20 +27,22 @@ def book_list(request):
             from libraryapp_book b
             """)
 
-            all_books = []
-            dataset = db_cursor.fetchall()
+            all_books = db_cursor.fetchall()
 
-            for row in dataset:
-                book = Book()
-                book.id = row['id']
-                book.title = row['title']
-                book.isbn = row['isbn']
-                book.author = row['author']
-                book.published = row['published']
-                book.librarian_id = row['librarian_id']
-                book.location_id = row['location_id']
+            # all_books = []
+            # dataset = db_cursor.fetchall()
 
-                all_books.append(book)
+            # for row in dataset:
+            #     book = Book()
+            #     book.id = row['id']
+            #     book.title = row['title']
+            #     book.isbn = row['isbn']
+            #     book.author = row['author']
+            #     book.published = row['published']
+            #     book.librarian_id = row['librarian_id']
+            #     book.location_id = row['location_id']
+
+            #     all_books.append(book)
 
         template = 'books/list.html'
         context = {
